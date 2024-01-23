ccaf8ad8282a74608faf8a5b9324d547145f2da1
sree-ragu <108447645+sree-ragu@users.noreply.github.com>
2024-01-23 08:32:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+
+# Load the trained model
+countVector  =pickle.load(open("count_vectorizer.pkl","rb"))
+model = pickle.load(open('model.pkl', 'rb'))  # Replace with your trained model
+
+# Application Frontend
+
+# Title of the Application
+st.title('Spam Classifier')
+
+# Text Input for the user
+text_input = st.text_area("Enter the text to classify:", "")
+
+# Classification Program
+if st.button('Classify'):
+    # Perform the classification
+    vector  = countVector.transform([text_input])
+    prediction = model.predict(vector)[0]
+
+    # Display the result
+    if prediction == 1:
+        st.write("This text is SPAM.")
+    else:
+        st.write("This text is NOT SPAM.")
